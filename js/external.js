67031128b2626ddc82ada7654799cf870810a0c3
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-02-02 14:44:51 -0600

    inline ,external javascript exercieses

---
+++ js/external.js
@@ -0,0 +1,23 @@
+console.log('hello from external javascript');
+
+alert('welcome to my site');
+
+//Prompt
+let favoriteColor = prompt('what is your favorite color?');
+console.log('your favorite color is ' + favoriteColor)
+
+
+    var littleMermaid = prompt('How many days did you rent the littleMermaid:');
+    console.log('The user entered' + littleMermaid);
+    var hercules = prompt('How many days did you rent the hercules:');
+    console.log('The user entered' + hercules);
+    var brotherBear = prompt('How many days did you rent the brotherBear:');
+    console.log('The user entered' + brotherBear);
+    var moviePrice = 3.00;
+    var total = ( littleMermaid ) * (moviePrice) + (hercules) * (moviePrice) + (brotherBear) * (moviePrice);
+    alert("My total price is $" + total)
+
+
+
+
+
---
+++ js/js-in-html.external.js
@@ -0,0 +1,17 @@
+"use strict";
+console.log('heller from js file');
+
+let personName = "Victor";
+console.log('hello' + personName );
+//alert
+alert('Welcome to my site');
+
+//Confirm
+let confirmLeave = confirm('are you sure you want to leave?');
+console.log('confirmLeave' + confirmLeave)
+
+//Prompt
+let favoriteColor = prompt('what is your favorite color?');
+console.log('your favorite color is ' + favoriteColor)
+
+
