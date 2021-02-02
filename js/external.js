58614f0b6b3dd6421e65034b304e7d122e63ce2c
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-02-02 16:42:18 -0600

    This is what I am commiting

--- js/external.js
+++ js/external.js
@@ -5,6 +5,7 @@ alert('welcome to my site');
 //Prompt
 let favoriteColor = prompt('what is your favorite color?');
 console.log('your favorite color is ' + favoriteColor)
+alert('Great, ' + favoriteColor + ' is my favorite color too!!');
 
 
     var littleMermaid = prompt('How many days did you rent the littleMermaid:');
@@ -16,6 +17,25 @@ console.log('your favorite color is ' + favoriteColor)
     var moviePrice = 3.00;
     var total = ( littleMermaid ) * (moviePrice) + (hercules) * (moviePrice) + (brotherBear) * (moviePrice);
     alert("My total price is $" + total)
+
+    var googleHours = prompt('How many hours did you work at google:');
+    console.log('The user entered' + googleHours);
+    var amazonHours = prompt('How many house did you work at amazon:');
+    console.log('The user entered' + amazonHours);
+    var faceBookHours = prompt('How many hours did you work at facebook:');
+    console.log('The user entered' + faceBookHours);
+    var gPay = 400;
+    var aPay= 380;
+    var fPay= 350;
+    var totalPay = (googleHours * gPay) + (amazonHours * aPay) + (faceBookHours * fPay);
+    alert("My total pay is $" + totalPay)
+
+
+
+
+
+
+
 
 
 
