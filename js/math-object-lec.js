11aa07e7b1c985825659949938ec6d991dc739a5
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-03-02 08:17:55 -0600

    circle exercise

---
+++ js/math-object-lec.js
@@ -0,0 +1,20 @@
+console.log("imhere")
+
+function getRandom (min,max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max  - min +1) + min);
+}
+
+//Math.round()
+console.log(Math.round(20.49));
+console.log(Math.round(20.5));
+console.log(Math.round(-20.5));
+console.log(Math.round(-20.50));
+
+//Math.cei(x)
+console.log("math.ceil 20.0008)", Math.ceil(20.0000))
+
+//Math.floor
+console.log("")
+
