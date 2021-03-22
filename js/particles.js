003d6e894c074a449e9a3ecb4b447da867fc9a4e
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-03-21 23:52:58 -0500

    particles

---
+++ js/particles.js
@@ -0,0 +1,14 @@
+      /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+      particlesJS.load('particles-js', 'particles.json', function() {
+        console.log('callback - particles.js config loaded');
+      });
+
+         /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+         particlesJS.load('particles-js2', 'particles.json', function() {
+            console.log('callback - particles.js config loaded');
+          });
+
+             /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+         particlesJS.load('particles-js3', 'particles2.json', function() {
+            console.log('callback - particles.js config loaded');
+          });
