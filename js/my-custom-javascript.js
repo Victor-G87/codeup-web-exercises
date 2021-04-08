80ca11c5fe71224b8b24947d79da3167303c8a38
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-04-08 15:37:18 -0500

    jquery

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,26 +1,26 @@
-
-    $(document).ready(function() {
-        // alert($('#heading-1').html());
-        // $("h1").hover(
-        //     function() {
-        //         var value = $(this).html();
-        //         $(this).html(value.toUpperCase());
-        //     },
-        //     function() {
-        //         var value = $(this).html();
-        //         $(this).html(value.toLowerCase());
-        //     }
-        // );
-        // $('h1').hover(
-        //     function() {
-        //         $(this).css('background-color', '#FF0');
-        //     },
-        //     function() {
-        //         $(this).css('background-color', '#FFF');
-        //     }
-        // );
-        $(document).onkeydown(function(e){
-            console.log('onkeydown');
-            console.log(e.keyCode);
-            console.log(e.key);
-    });
+    //
+    // $(document).ready(function() {
+    //     // alert($('#heading-1').html());
+    //     // $("h1").hover(
+    //     //     function() {
+    //     //         var value = $(this).html();
+    //     //         $(this).html(value.toUpperCase());
+    //     //     },
+    //     //     function() {
+    //     //         var value = $(this).html();
+    //     //         $(this).html(value.toLowerCase());
+    //     //     }
+    //     // );
+    //     // $('h1').hover(
+    //     //     function() {
+    //     //         $(this).css('background-color', '#FF0');
+    //     //     },
+    //     //     function() {
+    //     //         $(this).css('background-color', '#FFF');
+    //     //     }
+    //     // );
+    //     $(document).onkeydown(function(e){
+    //         console.log('onkeydown');
+    //         console.log(e.keyCode);
+    //         console.log(e.key);
+    // });
